5dc919a9edc8618f522bcac5134dd43394152617
Nicholas Schweitzer <f3bl@gmx.com>
2020-12-21 12:14:22 +0100

    Restructure and namespace

--- parse.hpp
+++ parse.hpp
@@ -1,45 +1,106 @@
+#ifndef CG_PARSE_H
+#define CG_PARSE_H
+
 #include <iostream>
 #include <vector>
 #include <sstream>
 #include <unordered_map>
 #include <algorithm>
 
-std::vector<std::string> OPERATORS = {"&&", "||", "!"};
+#include "expression.hpp"
 
-std::unordered_map<std::string, int> PRECEDENCE = {{"||", 20},
-						   {"&&", 50},
-						   {"!", 100},
-						   {"(", 999}};
+namespace parse {
+	std::unordered_map<std::string, expression::UnaryOp> UNARY_OPS = {{"!", expression::UnaryOp::Not}};
 
-std::vector<std::string> convert_to_postfix(std::string& in) {
-	std::vector<std::string> ops;
-	std::vector<std::string> out;
+	std::unordered_map<std::string, expression::BinaryOp> BINARY_OPS = {{"&&", expression::BinaryOp::And},
+								{"||", expression::BinaryOp::Or}};
 
-	std::istringstream stream(in);
-	for (std::string token; stream >> token;) {
-		auto is_binary_op = std::find(OPERATORS.begin(), OPERATORS.end(), token) != OPERATORS.end();
+	std::unordered_map<std::string, int> PRECEDENCE = {{"||", 20},
+							   {"&&", 50},
+							   {"!", 100}};
 
-		if (is_binary_op) {
-			while (ops.size() && PRECEDENCE[ops.back()] > PRECEDENCE[token] && ops.back() != "(") {
-				out.push_back(ops.back());
+	void print_expr(expression::Expr* expr, bool newline = true) {
+		if (auto e = std::get_if<expression::BaseExpr>(expr)) {
+			std::cout << e->name;
+		} else if (auto e = std::get_if<expression::UnaryExpr>(expr)) {
+			std::cout << '(';
+			for (auto [token, op] : UNARY_OPS) if (op == e->op) std::cout << token;
+
+			print_expr(e->a, false);
+			std::cout << ')';
+		} else if (auto e = std::get_if<expression::BinaryExpr>(expr)) {
+			std::cout << '(';
+			print_expr(e->a, false);
+			std::cout << ' ';
+			for (auto [token, op] : BINARY_OPS) if (op == e->op) std::cout << token;
+			std::cout << ' ';
+			print_expr(e->b, false);
+			std::cout << ')';
+		}
+	
+		if (newline) std::cout << std::endl;
+	}
+
+	std::vector<std::string> convert_to_postfix(std::string& in) {
+		std::vector<std::string> ops;
+		std::vector<std::string> out;
+
+		std::istringstream stream(in);
+		for (std::string token; stream >> token;) {
+			if (PRECEDENCE.find(token) != PRECEDENCE.end()) {
+				while (ops.size() && PRECEDENCE[ops.back()] > PRECEDENCE[token] && ops.back() != "(") {
+					out.push_back(ops.back());
+					ops.pop_back();
+				}
+				ops.push_back(token);
+			} else if (token == "(") {
+				ops.push_back(token);
+			} else if (token == ")") {
+				while (ops.back() != "(") {
+					out.push_back(ops.back());
+					ops.pop_back();
+				}
 				ops.pop_back();
-			}
-			ops.push_back(token);
-		} else if (token == "(") {
-			ops.push_back(token);
-		} else if (token == ")") {
-			while (ops.back() != "(") {
-				out.push_back(ops.back());
-				ops.pop_back();
-			}
+			} else out.push_back(token);
+		}
+
+		while (ops.size()) {
+			out.push_back(ops.back());
 			ops.pop_back();
-		} else out.push_back(token);
+		}
+
+		return out;
 	}
 
-	while (ops.size()) {
-		out.push_back(ops.back());
-		ops.pop_back();
-	}
+	// Expects tokens in postfix order. Heap-allocates all the exprs! Sorry :p
+	expression::Expr* tokens_to_expr(std::vector<std::string> tokens) {
+		std::vector<expression::Expr*> out;
+	
+		for (auto token : tokens) {
+			if (UNARY_OPS.find(token) != UNARY_OPS.end()) {
+				auto op = UNARY_OPS[token];
+				auto a = out.back(); out.pop_back();
+				auto e = new expression::Expr {std::in_place_index<1>, expression::UnaryExpr {op, a}};
+				out.push_back(e);
+			} else if (BINARY_OPS.find(token) != BINARY_OPS.end()) {
+				auto op = BINARY_OPS[token];
+				auto b = out.back(); out.pop_back();
+				auto a = out.back(); out.pop_back();
+				auto e = new expression::Expr {std::in_place_index<2>, expression::BinaryExpr {op, a, b}};
+				out.push_back(e);
+			} else {
+				auto e = new expression::Expr {std::in_place_index<0>, expression::BaseExpr {token}};
+				out.push_back(e);
+			}
+		}
 
-	return out;
+		if (out.size() != 1) {
+			printf("%lu items in out instead of 1!\n", out.size());
+			throw;
+		}
+
+		return out[0];
+	}
 }
+
+#endif // CG_PARSE_H
--- testing.cpp
+++ testing.cpp
@@ -1,65 +1,19 @@
 #include <iostream>
-#include <vector>
 
-#include "table-gen.hpp"
+#include "expression.hpp"
+#include "parse.hpp"
 
-const auto INTERVALS = 12;
-const auto OCTAVES = 3;
-const auto BASE = 1 << OCTAVES;
-const auto BITS = INTERVALS * OCTAVES;
-const uint64_t MAX = 1UL << BITS;
-const auto BUFSIZE = 4096;
-
-typedef uint64_t Chord;
-
-struct Condition {
-	Chord mask;
-
-	bool check_chord(Chord c) {
-		return (c & mask) > 0;
-	}
-};
-
-struct Rule {
-	std::vector<Condition> conditions;
-	std::vector<bool> truth_table;
-
-	bool check_chord(Chord c) {
-		uint64_t idx = 0;
-		for (auto cnd : conditions) idx = idx << 1 | cnd.check_chord(c);
-		return truth_table[idx];
-	}
-};
-
-void print_chord(Chord notes) {
-	for (auto octave = 0; octave < OCTAVES; ++octave) {
-		for (auto interval = 0; interval < INTERVALS; ++interval) {
-			auto interval_shift = (INTERVALS - interval - 1) * OCTAVES;
-			if (notes >> (interval_shift + octave) & 1)
-				std::cout << octave * INTERVALS + interval << ' ';
-		}
-	}
-	std::cout << std::endl;
-}
-
-void print_bitset(Chord b) {
-	for (auto i = 0; i < INTERVALS; ++i)
-		std::cout << (b >> ((INTERVALS - i - 1) * OCTAVES) & (BASE-1));
-	std::cout << std::endl;
-}
-
-int main() {
-	Condition m6 { 0'000000007000 };
-	Condition M6 { 0'000000000700 };
-	std::string expr_s = "A || B";
-	Expr expr(expr_s);
-	Rule rule {{m6, M6}, expr.gen_table()};
-
-	uint64_t ct = 0;
-	for (uint64_t i = 0; i < MAX; ++i) {
-		if (rule.check_chord(i)) ct++;
-		if (i % (1UL << 32) == 0) printf("%.1f%%\n", (double) i / (double) MAX * 100.0);
+int main(int argc, char *argv[]) {
+	if (argc < 2) {
+		printf("Not enough args\n");
+		exit(1);
 	}
 
-	std::cout << ct << " match." << std::endl;
+	std::string input(argv[1]);
+	auto tokens = parse::convert_to_postfix(input);
+	auto expr = parse::tokens_to_expr(tokens);
+
+	parse::print_expr(expr);
+	auto table = expression::gen_table(expr);
+	expression::print_table(table);
 }
